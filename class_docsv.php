5a906974931f7adbc4611ef49778ab57250d2b3f
c4pone <mail@codebuster.de>
2014-08-21 19:52:44 +0930

    Class is now trying to download the file a in the class defined number of times

--- class_docsv.php
+++ class_docsv.php
@@ -1,83 +1,121 @@
 <?php
 
 class doCSV {
-    /**
-     * This is the job that will be launched by the Laravel queue...
-     *
-     * @param Illuminate\Queue\Jobs\Job $job class retrieved from Laravel's queue
-     * @param array $data the data that is going to be used for this queue call.
-     *
-     */
-    public function runJob($job, $data)
-    {
-        $downloadUrl = $data['url'];
-        $clientID = (int) $data['client'];
-        try{
-           $file = $this->downloadCSV($downloadUrl);
-           $success = $this->insertCSVIntoDatabase($clientID, $file);
-        }catch(Exception $ex){
-           return false;
-        }
-    }
+   private $maxDownloadTries = 10;
+   private $currentTries = 0;
+   /**
+    * This is the job that will be launched by the Laravel queue...
+    *
+    * @param Illuminate\Queue\Jobs\Job $job class retrieved from Laravel's queue
+    * @param array $data the data that is going to be used for this queue call.
+    *
+    */
+   public function runJob($job, $data)
+   {
+      $downloadUrl = $data['url'];
+      $clientID = (int) $data['client'];
+      $file = null;
+      $tryDownloadFile = true;
 
-    /**
-     * Here we have our CSV file that has been downloaded. We need to do the following:
-     *
-     *      * uncompress the gzip file
-     *      * store that uncompressed file somewhere
-     *      * go row by row and insert the data into a database.
-     *
-     *  Important notes:
-     *
-     *      * the gzip files could be 250MB big
-     *      * uncompressed files more then 2GB big
-     *      * we want to use as little RAM as possible ;)
-     *      * we deploy code with a memory limit of 512MB ... not enough ram to
-     *        store all of the CSV file or rows to insert in the database in one go.
-     *
-     *  Bonus notes:
-     *
-     *      * chunking inserts into the database would be nice
-     *
-     *      * str_getcsv has some bugs. as a hint, check out the PHP documentation,
-     *        and read the comment from Ryan Rubley. These bugs will be hit with well-formed
-     *        CSV files. The file provided will not expose the bugs.
-     *
-     *      * fopen, fseek and gzopen will be your friends.
-     *
-     * @param int $clientID the ID of the client that the CSV is for
-     * @param string $file  the file that we are going to be importing.
-     *
-     */
-    private function insertCSVIntoDatabase($clientID, $file)
-    {
-        throw new Exception("There was an error importing the CSV file into the database.");
-    }
+      while($tryDownloadFile){
+         try{
+            $file = $this->downloadCSV($downloadUrl);
+         }catch(Exception $ex){
+            $this->currentTries++;
+            if($this->currentTries >= $this->maxDownloadTries){
+               $tryDownloadFile = false; 
+            }
+         }
+      }
 
-    /**
-     * This is a function that will download a file using curl over HTTP.
-     *
-     * For the moment, we will just return the path of the compressed file.
-     * There is no need to download the CSV file at all \o/
-     *
-     * @param string $downloadUrl the URL to download the file from.
-     *
-     */
-    private function downloadCSV($downloadUrl)
-    {
-        $test = round(rand(1, 6));
-        echo $test;
+      $this->extractGzipFile($file, __DIR__.'/batch_of_urls.csv');
+      $success = $this->insertCSVIntoDatabase($clientID, $file);
+   }
 
-        // We are going to throw a random exception sometimes...
-        if ($test == 3)
-        {
-            throw new Exception("Could not pretend to download file");
-        }
+   /**
+    * 
+    * Extract a gzip file 
+    *
+    * @param String $src Destination of gzip file
+    * @param String $dst Path to output file
+    * @param int    $chunkSize Size of chunks that get read every loop
+    *
+    */
+   private function extractGzipFile($src, $dst, $chunkSize = 4096)
+   {
+      if($src === '' OR $src === NULL OR $dst === '' OR $dst === NULL)
+         return false;
 
-        $_file = __DIR__ . '/batch_of_urls.csv.gz';
-        if (!file_exists($_file)) {
-            throw new Exception("Oh noes, it looks like your CSV file is missing");
-        }
-    }
+      $srcHandler = gzopen($src, "rb");
+      $dstHandler = fopen($dst, "w");
+
+      while (!gzeof($srcHandler)) {
+         $string = gzread($srcHandler, $chunkSize);
+         fwrite($dstHandler, $string, strlen($string));
+      }
+      gzclose($srcHandler);
+      fclose($dstHandler);
+   }
+
+
+   /**
+    * Here we have our CSV file that has been downloaded. We need to do the following:
+    *
+    *      * uncompress the gzip file
+    *      * store that uncompressed file somewhere
+    *      * go row by row and insert the data into a database.
+    *
+    *  Important notes:
+    *
+    *      * the gzip files could be 250MB big
+    *      * uncompressed files more then 2GB big
+    *      * we want to use as little RAM as possible ;)
+    *      * we deploy code with a memory limit of 512MB ... not enough ram to
+    *        store all of the CSV file or rows to insert in the database in one go.
+    *
+    *  Bonus notes:
+    *
+    *      * chunking inserts into the database would be nice
+    *
+    *      * str_getcsv has some bugs. as a hint, check out the PHP documentation,
+    *        and read the comment from Ryan Rubley. These bugs will be hit with well-formed
+    *        CSV files. The file provided will not expose the bugs.
+    *
+    *      * fopen, fseek and gzopen will be your friends.
+    *
+    * @param int $clientID the ID of the client that the CSV is for
+    * @param string $file  the file that we are going to be importing.
+    *
+    */
+   private function insertCSVIntoDatabase($clientID, $file)
+   {
+      throw new Exception("There was an error importing the CSV file into the database.");
+   }
+
+   /**
+    * This is a function that will download a file using curl over HTTP.
+    *
+    * For the moment, we will just return the path of the compressed file.
+    * There is no need to download the CSV file at all \o/
+    *
+    * @param string $downloadUrl the URL to download the file from.
+    *
+    */
+   private function downloadCSV($downloadUrl)
+   {
+      $test = round(rand(1, 6));
+      echo $test;
+
+      // We are going to throw a random exception sometimes...
+      if ($test == 3)
+      {
+         throw new Exception("Could not pretend to download file");
+      }
+
+      $_file = __DIR__ . '/batch_of_urls.csv.gz';
+      if (!file_exists($_file)) {
+         throw new Exception("Oh noes, it looks like your CSV file is missing");
+      }
+   }
 
 }
--- tests/DocsvTest.php
+++ tests/DocsvTest.php
@@ -27,9 +27,26 @@ class DocsvTest extends TestCase {
          $project->runJob(null,$data);
          $project->runJob(null,$data);
          $project->runJob(null,$data);
-      
+
       }catch(Exception $ex){
          $this->assertNotEquals('Could not pretend to download file', $ex->getMessage());
+      }
+   }
+
+   public function testExtractGzip()
+   {
+      if(file_exists('batch_of_urls.csv')){
+         unlink('batch_of_urls.csv');
+      }
+
+      $project = new doCSV();
+      $data = array (
+         'url'   => '',
+         'client'=> 123
+      );
+      $result = $project->runJob(null,$data);
+      if($result !== false){
+         $this->assertTrue(file_exists('batch_of_urls.csv'));
       }
    }
 }
